a45001474d9451760788a3240c985f8a790ea5a0
Stephanie <stephaniiehallberg@gmail.com>
2023-05-05 06:14:16 +0200

    feat(upload): upload folder + main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,15 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 
-	if (process.env.NODE_ENV === 'development') {
-		const corsOptions: CorsOptions = {
-			origin: 'http://localhost:3000', // Replace with the appropriate origin URL for development
-			methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
-			allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed request headers
-			credentials: true, // Enable credentials (e.g., cookies, authorization headers) to be sent in cross-origin requests
-		}
+	const corsOptions: CorsOptions = {
+		origin: process.env.PUBLIC_URL, // Replace with the appropriate origin URL for development
+		methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+		allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed request headers
+		credentials: true, // Enable credentials (e.g., cookies, authorization headers) to be sent in cross-origin requests
 	}
+
+	app.enableCors(corsOptions)
+
 	await app.listen(process.env.PORT)
 }
 bootstrap()
